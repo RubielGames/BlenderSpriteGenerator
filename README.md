f7d5a78ff67becdc9f31ab10b5f2db1a17647479
Rubiel <rubiel@rubielgames.com>
2023-11-17 17:04:13 +0100

    Added option to use your own camera instead of generated one. When this camera is used, part of parameters are disabled. Updated documentation README to reflect the change.

--- README.md
+++ README.md
@@ -1,28 +1,36 @@
-# 2D/2.5D Sprite Renderer for Blender
+# 2D/2.5D Sprite Renderer for Blender 3.x
 
-The 2D/2.5D Sprite Renderer is a Blender addon that allows users to automatically render a 3D model from multiple angles. This is particularly useful for generating 2D sprites for use in 2D or 2.5D games.
+The 2D/2.5D Sprite Renderer is a Blender addon designed to render 3D models from many angles. It's ideal for creating 2D sprites for 2D and 2.5D games.
 
 ## Installation
-1. Download the addon from GitHub as a .zip file. (Click green button "Code" and then "Download ZIP")
-2. In Blender, navigate to Edit > Preferences.
-3. In the Addons tab, click Install... to open a file dialog.
-4. Navigate to where you saved the .zip file, select it, and click Install Add-on.
-5. The addon should now appear in the list of addons. Check the box next to its name to enable it.
+1. Download the addon from GitHub as a .zip file (click "Code" then "Download ZIP").
+2. In Blender, go to Edit > Preferences.
+3. In the Addons tab, select Install... to open a file dialog.
+4. Find and select the .zip file you downloaded, then click Install Add-on.
+5. The addon should now be in the list of addons. Enable it by checking the box next to its name.
 
 ## Usage
-1. Open the 3D view sidebar (press N if it's not visible), and locate the "Sprite Renderer" panel.
-2. Set the parameters for rendering:
-    * `Character Height`: The height of the character in the scene.
-    * `Number of Angles`: The number of angles from which the character will be rendered.
-    * `Output Folder`: The directory where the rendered images will be saved.
-    * `Basename`: The base name for the rendered image files.
-    * `Perspective`: Choose between 2D and 2.5D. In 2.5D mode, you can specify the angle of the camera.
-    * `Camera Angle`: The angle of the camera when rendering the scene. This is only applicable for 2.5D perspective.
-    * `Camera Type`: Select between Orthographic and Perspective.
-    * `Resolution`: The resolution of the output render.
-    * `Include Animation`: Check if you want to render the character's animation.
-    * `Frame Step`: If animation is included, you can specify the frame step to render every n-th frame.
-3. After setting the parameters, click on Render Sprites to start the rendering process. This will create a sprite image for each angle in the specified output directory.
+1. Open the 3D view sidebar (press N if hidden), and find the "Sprite Renderer" panel.
+2. Set rendering parameters:
+    * `Camera`: Choose a camera for rendering. Leave empty to use camera(s) parameters.
+    * `Output Folder`: Directory for saving rendered images.
+    * `Basename`: Base name for image files.
+    * `Include Animation`: Enable to render animations.
+    * `Frame Step`: If using animation, define the frame step for rendering.
+    * `Character Height`: Set the character's height in the scene.
+    * `Number of Angles`: Define how many angles to render the character from.
+    * `Perspective`: Choose 2D or 2.5D. In 2.5D, set the camera angle.
+    * `Camera Angle`: Set the camera angle for 2.5D perspective.
+    * `Camera Type`: Choose Orthographic or Perspective.
+    * `Resolution`: Set the output render resolution.
+3. Click Render Sprites to start. This creates a sprite image for each angle in the output directory.
 
-## Note
-When using this addon, make sure that the character is at the origin of the scene (`0, 0, 0`). The addon will position the camera around the character based on the given parameters and render the scene from each camera position.
+## Usage - No Camera Selected
+This is the default setting. Ensure the character is at the origin (`0, 0, 0`). The addon will position cameras around the character based on your parameters. Typical top-down game settings are a `camera angle` of 30 to 60, `perspective` at 2.5, and `camera type` as Orthographic.
+
+## Usage - Your Own Camera
+This will disable Camera Settings box. Animation is still usable, ideal for rendering effects or non-static movements. For a camera style like the default:
+    * Select your camera.
+    * Go to the `Data` tab, expand `Lens`, and set `Type` to Orthographic.
+    * In the `Output` tab, adjust `Resolution` in the Format box.
+    * Press `F12` to test rendering.
--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "2D/2.5D Sprite Renderer",
     "description": "Automatically renders a character from multiple angles for use in 2D or 2.5D games",
     "author": "Rubiel",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (3, 4, 0),
     "location": "View3D > Sidebar > Sprite Renderer Panel",
     "category": "Object",
@@ -21,84 +21,100 @@ class CharacterSpriteRenderer(bpy.types.Operator):
 
     def execute(self, context): 
 
-        # Getting properties from the scene
-        character_position = Vector((0, 0, 0)) 
-        character_height = context.scene.sprite_renderer.character_height
-        number_of_angles = context.scene.sprite_renderer.number_of_angles
-
+        # Check if the camera path is valid
+        camera_path = context.scene.sprite_renderer.camera_path
         output_folder = bpy.path.abspath(context.scene.sprite_renderer.output_folder)
         os.makedirs(output_folder, exist_ok=True)
         basename = context.scene.sprite_renderer.basename
         scene = bpy.context.scene
-        scene.render.resolution_x = int(context.scene.sprite_renderer.resolution)
-        scene.render.resolution_y = int(context.scene.sprite_renderer.resolution)
-        scene.render.film_transparent = True
         include_animation = context.scene.sprite_renderer.include_animation
         frame_step = context.scene.sprite_renderer.frame_step
-
-        perspective = context.scene.sprite_renderer.perspective
-        camera_type = context.scene.sprite_renderer.camera_type
-
-        # Check if the camera already exists in the scene
-        camera_name = '2DSpriteCamera'
-        if camera_name in bpy.data.objects:
-            camera = bpy.data.objects[camera_name]
-        else:
-            # Add a new camera to the scene
-            bpy.ops.object.camera_add(location=character_position)
-            camera = bpy.context.object  # the newly added camera becomes the active object
-            camera.name = camera_name  # set the camera name
-        scene.camera = camera
-
-
-        if camera_type == 'ORTHO':
-            camera.data.type = 'ORTHO'
-            camera.data.ortho_scale = character_height * 1.1
-        else:
-            camera.data.type = 'PERSP'
-
-        if perspective == '2D':
-            distance = 10
-            z_distance = 0
-        else:
-            distance = 0.5 * character_height / tan(0.5 * atan(0.5 * camera.data.sensor_height / camera.data.lens))
-            camera_angle_rad = context.scene.sprite_renderer.camera_angle * 3.14159 / 180  # converting to radians
-            z_distance = distance * tan(camera_angle_rad)
-
         # Get the frame range of the animation
         frame_start = scene.frame_start
         frame_end = scene.frame_end
 
-        # Loop to set the camera's position and render the scene
-        for i in range(number_of_angles):
-            angle = i * (2.0 * 3.14159 / number_of_angles)  # calculate angle
 
-            # Set the camera's position
-            camera.location.x = character_position.x + distance * cos(angle)
-            camera.location.y = character_position.y + distance * sin(angle)
-            camera.location.z = character_position.z + (character_height / 2) + z_distance
+        if camera_path == "":
+            # Getting properties from the scene
+            character_position = Vector((0, 0, 0)) 
+            character_height = context.scene.sprite_renderer.character_height
+            number_of_angles = context.scene.sprite_renderer.number_of_angles
 
-            # Point the camera to the character
-            direction = Vector((character_position.x, character_position.y, character_position.z + (character_height / 2))) - camera.location
-            camera.rotation_mode = 'QUATERNION'
-            camera.rotation_quaternion = direction.to_track_quat('-Z', 'Y')  # camera looks towards the character
+            scene.render.resolution_x = int(context.scene.sprite_renderer.resolution)
+            scene.render.resolution_y = int(context.scene.sprite_renderer.resolution)
+            scene.render.film_transparent = True
 
-            # Check if there is animation
+            perspective = context.scene.sprite_renderer.perspective
+            camera_type = context.scene.sprite_renderer.camera_type
+
+            # Check if the camera already exists in the scene
+            camera_name = '2DSpriteCamera'
+            if camera_name in bpy.data.objects:
+                camera = bpy.data.objects[camera_name]
+            else:
+                # Add a new camera to the scene
+                bpy.ops.object.camera_add(location=character_position)
+                camera = bpy.context.object  # the newly added camera becomes the active object
+                camera.name = camera_name  # set the camera name
+            scene.camera = camera
+
+            if camera_type == 'ORTHO':
+                camera.data.type = 'ORTHO'
+                camera.data.ortho_scale = character_height * 1.1
+            else:
+                camera.data.type = 'PERSP'
+
+            if perspective == '2D':
+                distance = 10
+                z_distance = 0
+            else:
+                distance = 0.5 * character_height / tan(0.5 * atan(0.5 * camera.data.sensor_height / camera.data.lens))
+                camera_angle_rad = context.scene.sprite_renderer.camera_angle * 3.14159 / 180  # converting to radians
+                z_distance = distance * tan(camera_angle_rad)
+
+            # Loop to set the camera's position and render the scene
+            for i in range(number_of_angles):
+                angle = i * (2.0 * 3.14159 / number_of_angles)  # calculate angle
+
+                # Set the camera's position
+                camera.location.x = character_position.x + distance * cos(angle)
+                camera.location.y = character_position.y + distance * sin(angle)
+                camera.location.z = character_position.z + (character_height / 2) + z_distance
+
+                # Point the camera to the character
+                direction = Vector((character_position.x, character_position.y, character_position.z + (character_height / 2))) - camera.location
+                camera.rotation_mode = 'QUATERNION'
+                camera.rotation_quaternion = direction.to_track_quat('-Z', 'Y')  # camera looks towards the character
+
+                # Check if there is animation
+                if include_animation and frame_start != frame_end:
+                    # Loop through every frame of the animation
+                    for frame in range(frame_start, frame_end + 1, frame_step):
+                        scene.frame_set(frame)
+                        # Set output path
+                        scene.render.filepath = os.path.join(output_folder, '{}_angle{}_frame{:03}.png'.format(basename, i, frame))
+                        # Render the scene
+                        bpy.ops.render.render(write_still = True)
+                else:
+                    # Handle static scene (no animation)
+                    scene.render.filepath = os.path.join(output_folder, '{}_angle{}.png'.format(basename, i))
+                    # Render the scene
+                    bpy.ops.render.render(write_still = True)
+
+            bpy.data.objects.remove(camera, do_unlink=True)
+        else:
+            scene.camera = bpy.data.objects[camera_path]
             if include_animation and frame_start != frame_end:
                 # Loop through every frame of the animation
                 for frame in range(frame_start, frame_end + 1, frame_step):
                     scene.frame_set(frame)
-                    # Set output path
-                    scene.render.filepath = os.path.join(output_folder, '{}_angle{}_frame{}.png'.format(basename, i, frame))
-                    # Render the scene
+                    scene.render.filepath = os.path.join(output_folder, '{}_frame{:03}.png'.format(basename, frame))
                     bpy.ops.render.render(write_still = True)
             else:
                 # Handle static scene (no animation)
-                scene.render.filepath = os.path.join(output_folder, '{}_angle{}.png'.format(basename, i))
+                scene.render.filepath = os.path.join(output_folder, '{}.png'.format(basename))
                 # Render the scene
                 bpy.ops.render.render(write_still = True)
-
-        bpy.data.objects.remove(camera, do_unlink=True)
 
         return {'FINISHED'}            # Lets Blender know the operator finished successfully.
 
@@ -118,23 +134,36 @@ class SpriteRendererPanel(bpy.types.Panel):
         scene = context.scene
         renderer = scene.sprite_renderer
 
+        layout.prop(renderer, "camera_path")
         # draw the properties
-        layout.prop(renderer, "character_height")
-        layout.prop(renderer, "number_of_angles")
         layout.prop(renderer, "output_folder")
         layout.prop(renderer, "basename")
-        layout.prop(renderer, "perspective")
-        # Only enable camera_angle if perspective is '2.5D'
-        row = layout.row()
-        row.enabled = (renderer.perspective == '2.5D')
-        row.prop(renderer, "camera_angle")
-        layout.prop(renderer, "camera_type")
-        layout.prop(renderer, "resolution")
+
         layout.prop(renderer, "include_animation")
         # Only enable frame_step if include_animation is True
         row = layout.row()
         row.enabled = renderer.include_animation
         row.prop(renderer, "frame_step")
+
+        # Camera settings subsection
+        camera_box = layout.box()
+        # Disable the entire camera settings box if camera_path is not empty
+        camera_box.enabled = (renderer.camera_path == "")
+
+        # Adding a label for the subsection
+        camera_box.label(text="Camera Settings")
+
+        camera_box.prop(renderer, "character_height")
+        camera_box.prop(renderer, "number_of_angles")
+
+        camera_box.prop(renderer, "perspective")
+
+        row = camera_box.row()
+        row.enabled = (renderer.perspective == '2.5D')
+        row.prop(renderer, "camera_angle")
+
+        camera_box.prop(renderer, "camera_type")
+        camera_box.prop(renderer, "resolution")
 
         # draw the operator
         layout.operator("object.sprite_renderer")
@@ -143,10 +172,24 @@ class SpriteRendererPanel(bpy.types.Panel):
 # This is the property group where you store your variables
 class SpriteRendererProperties(bpy.types.PropertyGroup):
 
-    character_height: bpy.props.FloatProperty(name="Character Height", default=2.0)
-    number_of_angles: bpy.props.IntProperty(name="Number of Angles", default=8)
+    camera_path: bpy.props.StringProperty(name="Camera", description="Path to an existing camera, if none is provided a new one will be created", default="")
     output_folder: bpy.props.StringProperty(name="Output Folder", default="//", subtype='DIR_PATH')
     basename: bpy.props.StringProperty(name="Basename", default="render")
+
+    include_animation: bpy.props.BoolProperty(
+        name="Include Animation",
+        default=False,
+        description="Whether to render all frames of the animation for each angle.",
+    )
+    frame_step: bpy.props.IntProperty(
+        name="Frame Step",
+        default=1,
+        min=1,
+        description="Render every n-th frame of the animation. For example, if set to 3, only every third frame will be rendered.",
+    )
+
+    character_height: bpy.props.FloatProperty(name="Character Height", default=2.0)
+    number_of_angles: bpy.props.IntProperty(name="Number of Angles", default=8)
     perspective: bpy.props.EnumProperty(
         name="Perspective",
         description="Select Perspective type",
@@ -177,17 +220,6 @@ class SpriteRendererProperties(bpy.types.PropertyGroup):
             ('512', "512x512", "")
         ],
         default='512',
-    )
-    include_animation: bpy.props.BoolProperty(
-        name="Include Animation",
-        default=False,
-        description="Whether to render all frames of the animation for each angle.",
-    )
-    frame_step: bpy.props.IntProperty(
-        name="Frame Step",
-        default=1,
-        min=1,
-        description="Render every n-th frame of the animation. For example, if set to 3, only every third frame will be rendered.",
     )
 
 
